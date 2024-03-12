9fc7231d536c364a781e6f65c9c812b291d06e7e
Apoorvjain09 <113705783+Apoorvjain09@users.noreply.github.com>
2024-03-12 19:08:45 +0530

    Update layout.tsx

--- app/(base-layout)/layout.tsx
+++ app/(base-layout)/layout.tsx
@@ -2,6 +2,12 @@ import { ReactNode } from "react";
 import NavBar from "./_components/shared/navBar/NavBar";
 import Footer from "./_components/shared/footer/Footer";
 
+export const metadata: Metadata = {
+  title: "Toefl Voucher",
+  description:
+    "Get TOEFL voucher code and save ₹ 2005 on your TOEFL registration fee. The TOEFL voucher is completly FREE of cost for everyone.",
+};
+
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="">
@@ -13,3 +19,4 @@ const layout = ({ children }: { children: ReactNode }) => {
 };
 
 export default layout;
+
