827f3c45cb22a136ce2892067c2ea15d1c386f50
Apoorvjain09 <113705783+Apoorvjain09@users.noreply.github.com>
2024-04-04 00:28:05 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,49 @@
-import { Metadata, MetadataRoute } from 'next'
-import React from 'react'
+import { Metadata, MetadataRoute } from 'next';
+import React from 'react';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
-        url: "https://toefldiscountcode.com/"
+      url: "https://toefldiscountcode.com/"
     },
     {
-        url: "https://toefldiscountcode.com/discover"
+      url: "https://toefldiscountcode.com/discover"
     },
     {
-        url: "https://toefldiscountcode.com/discover/toefl-ets"
+      url: "https://toefldiscountcode.com/discover/toefl-guide"
+    },
+    {
+      url: "https://toefldiscountcode.com/discover/toefl-exam"
+    },
+    {
+      url: "https://toefldiscountcode.com/discover/toefl-full-form"
+    },
+    {
+      url: "https://toefldiscountcode.com/discover/toefl-exam-fee"
+    },
+    {
+      url: "https://toefldiscountcode.com/discover/toefl-login"
+    },
+    {
+      url: "https://toefldiscountcode.com/discover/toefl-ets"
+    },
+    {
+      url: "https://toefldiscountcode.com/discover/toefl-exam-pattern"
+    },
+    {
+      url: "https://toefldiscountcode.com/discover/toefl-ets-login"
+    },
+    {
+      url: "https://toefldiscountcode.com/discover/toefl-ibt"
+    },
+    {
+      url: "https://toefldiscountcode.com/discover/toefl-vs-ielts"
+    },
+    {
+      url: "https://toefldiscountcode.com/discover/toefl-practice-tests"
+    },
+    {
+      url: "https://toefldiscountcode.com/discover/toefl-and-ielts"
     }
   ]
 }
