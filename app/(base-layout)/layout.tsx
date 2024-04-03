2a47698b05179f951dd17440c5e7d1694e391fc7
Apoorvjain09 <113705783+Apoorvjain09@users.noreply.github.com>
2024-04-04 00:35:55 +0530

    Update layout.tsx

--- app/(base-layout)/layout.tsx
+++ app/(base-layout)/layout.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from "react";
 import NavBar from "./_components/shared/navBar/NavBar";
 import Footer from "./_components/shared/footer/Footer";
+import { Metadata } from 'next';
 
-// export const metadata: Metadata = {
-//   title: "Toefl Voucher",
-//   description:
-//     "Get TOEFL voucher code and save ₹ 2005 on your TOEFL registration fee. The TOEFL voucher is completly FREE of cost for everyone.",
-// };
+export const metadata: Metadata = {
+  title: "Toefl Discount Code",
+  description:
+    "Get FREE Toefl discount codes for 2024 on your TOEFL Registration. Avail a INR 2000 Discount on the TOEFL iBT fees. Check Code Here.",
+};
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
