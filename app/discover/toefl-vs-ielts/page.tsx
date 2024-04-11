975e80b14c6df352248a71fea009ebe4e9a598d3
Apoorvjain09 <113705783+Apoorvjain09@users.noreply.github.com>
2024-04-11 18:52:20 +0530

    Update page.tsx

--- app/discover/toefl-vs-ielts/page.tsx
+++ app/discover/toefl-vs-ielts/page.tsx
@@ -7,7 +7,7 @@ import Head from 'next/head'
 export default function TOEFLvsIELTS() {
     return (
         <Head>
-            <title>Our Blog - Discover TOEFL Guides and Tips</title>
+            <title>TOEFL Vs IELTS Idp</title>
             <meta name="description" content="Explore comprehensive guides and tips for the TOEFL exam. Get insights on preparation, patterns, fees, and more to ace your test." />
             <meta name="keywords" content="TOEFL, TOEFL Exam, TOEFL Tips, TOEFL Preparation, TOEFL Guide" />
         </Head>
